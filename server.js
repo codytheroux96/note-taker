d3ebdf4032909fc19f8bb123d900a149c6e665f4
cdt96 <cody.theroux3@gmail.com>
2022-06-20 15:27:22 -0400

    added my dependencies, express data parsing, and a listener event

---
+++ server.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () =>
+  console.info(`Example app listening at http://localhost:${PORT} 🚀`)
+);
