1b06b8aeda9c717990d1844af4eb66be0c98bc37
cdt96 <cody.theroux3@gmail.com>
2022-06-20 15:37:51 -0400

    added my const for notes which requires the db.json

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = 3001;
+const notes = require("./db/db.json")
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
