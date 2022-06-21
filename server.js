73d6524b4f841821e5c62197c2a6dd4117ac62f4
cdt96 <cody.theroux3@gmail.com>
2022-06-21 14:10:20 -0400

    changed the app.post im working on in the comments

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT =  3001;
+const PORT = 3001;
 const { notes } = require("./db/db.json");
 const { uuid } = require("./utils/utils.js");
 
@@ -12,64 +12,64 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-  });
-  
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-  });
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
-  });
-  
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
 app.get("/api/notes", (req, res) => {
-    res.json(notes);
-  });
-  
-  // function createNewNote(note, notesArray) {
-  //   const { title, text} = note;
-  //   const notesArray = [];
-  //   let newNote = {
-  //     title,
-  //     text,
-  //     id: uuid(),
-  //   };
-  //   if (title && text)
-  //   notesArray.push(newNote);
-  //   fs.writeFileSync(
-  //     path.join(__dirname, './db/db.json'),
-  //     JSON.stringify({ notes: notesArray }, null, 2)
-  //   );
-  //   return newNote;
-  // };  
-  
+  res.json(notes);
+});
+
+// function createNewNote(note, notesArray) {
+//   const { title, text} = note;
+//   const notesArray = [];
+//   let newNote = {
+//     title,
+//     text,
+//     id: uuid(),
+//   };
+//   if (title && text)
+//   notesArray.push(newNote);
+//   fs.writeFileSync(
+//     path.join(__dirname, './db/db.json'),
+//     JSON.stringify({ notes: notesArray }, null, 2)
+//   );
+//   return newNote;
+// };  
+
 // app.post('/api/notes', (req, res) => {
 // });
-  
+
 
 
 // app.post('/api/notes', (req, res) => {
 //   const { title, text, } = req.body;
-//   if (title && text ) {
+//   if (title && text) {
 //     const newNote = {
 //       title,
 //       text,
 //       review_id: uuid(),
 //     };
 //     const noteString = JSON.stringify(newNote);
-//     fs.writeFile(`./db/db.json`, noteString, (err) =>{}
-//     );
+//     fs.writeFile(`./db/db.json`, noteString, () => {
+//      const response = {
+//         status: 'success',
+//         body: newNote,
+//       }
+//     })
+//   };
+// })
 
-//     const response = {
-//       status: 'success',
-//       body: newReview,
-//     }};
-//   });
-  
 
-  //need to make a function to create a new note
-  //need to make an app.post to post the new note
+//need to make a function to create a new note
+//need to make an app.post to post the new note
 
 
 
