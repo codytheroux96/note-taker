4f31305d49b626acfeeddba7d46af095d02001f5
cdt96 <cody.theroux3@gmail.com>
2022-06-21 09:26:05 -0400

    added and app.get / and turned notes into an object in my db file as well as added curly brackets to notes in line 6 of my server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = 3001;
-const notes = require("./db/db.json")
+const { notes } = require("./db/db.json")
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -21,6 +21,11 @@ app.get("/api/notes", (req, res) => {
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
   });
+
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
+
 
   //need to create a unique id
   //need to make a function to create a new note
